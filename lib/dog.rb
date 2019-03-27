035a5d5180dc0bfa5a526874060405998df899e7
David Stewart <davidstewart@gmail.com>
2019-03-27 04:03:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,10 @@ class Person
 end
 
 class Dog
-  def initialize(name,breed)
+  def initialize(name,breed = "Mutt")
     @name = name
-    if breed
     @breed = breed
-  else
-    @breed = "Mutt"
-  end
+ 
   end
   
 end
